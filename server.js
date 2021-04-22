35c9b3a31e05aeaac7538d1804adfa26893272bb
kayvonk <kayvonk@gmail.com>
2021-04-22 00:21:29 -0600

    updated server file with process env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
 const PORT = 3000;
 
@@ -15,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
